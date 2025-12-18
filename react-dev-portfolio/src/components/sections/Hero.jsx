d4c8a04fa098a71c164b4519561ef6afbeb3e253
sageethhimachala <sageethhimachala@gmail.com>
2025-12-18 10:05:14 +0530

    backgound added

--- react-dev-portfolio/src/components/sections/Hero.jsx
+++ react-dev-portfolio/src/components/sections/Hero.jsx
@@ -15,7 +15,7 @@ import RadialBackground from "../backgrounds/RadialBackground";
 function Hero() {
   return (
     <section className="relative min-h-screen flex items-center overflow-hidden bg-black">
-      {/* <RadialBackground variant="hero" /> */}
+      <RadialBackground variant="hero" />
 
       {/* Content */}
       <div className=" relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20 w-full">
@@ -123,6 +123,14 @@ function Hero() {
           </FadeIn>
         </div>
       </div>
+
+      {/* Scroll Down Indicator */}
+      <button
+        onClick={() => scrollToSection("about")}
+        className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce"
+      >
+        <ChevronDown className="w-8 h-8 text-primary" />
+      </button>
     </section>
   );
 }
