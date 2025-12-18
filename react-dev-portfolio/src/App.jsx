efca7d624e9869546f6d1668f8e4f243e6fd0a74
sageethhimachala <sageethhimachala@gmail.com>
2025-12-18 22:34:01 +0530

    added services structure

--- react-dev-portfolio/src/App.jsx
+++ react-dev-portfolio/src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/sections/Hero";
 import About from "./components/sections/About";
 import Skills from "./components/sections/Skills";
 import Projects from "./components/sections/Projects";
+import Services from "./components/sections/Services";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <About />
         <Skills />
         <Projects />
+        <Services />
       </main>
     </div>
   );
